b1ac8d2fadc60855ab71eff0db9388e62ff83277
Andreas Böttger <andreas.boettger@tuhh.de>
2017-12-05 15:51:39 +0100

    1.6.1

--- 1.6.1/entrypoint.sh
+++ 1.6.1/entrypoint.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
-#set -e
+set -e
 
-if [ -z "$MYSQL_PORT_3306_TCP_ADDR" ]; then
-	echo >&2 'error: missing MYSQL_PORT_3306_TCP environment variable'
-	echo >&2 '  Did you forget to --link some_mysql_container:mysql ?'
-
-	#exit 1
-fi
-
-# if we're linked to MySQL, and we're using the root user, and our linked
-# container has a default "root" password set up and passed through... :)
+: ${ETHERPAD_DB_TYPE:=mysql}
+: ${ETHERPAD_DB_HOST:=mysql}
 : ${ETHERPAD_DB_USER:=root}
+: ${ETHERPAD_DB_NAME:=etherpad}
+: ${ETHERPAD_DB_CHARSET:=utf8mb4}
+ETHERPAD_DB_NAME=$( echo $ETHERPAD_DB_NAME | sed 's/\./_/g' )
+
+# ETHERPAD_DB_PASSWORD is mandatory in mysql container, so we're not offering
+# any default. If we're linked to MySQL through legacy link, then we can try
+# using the password from the env variable MYSQL_ENV_MYSQL_ROOT_PASSWORD
 if [ "$ETHERPAD_DB_USER" = 'root' ]; then
 	: ${ETHERPAD_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
 fi
-: ${ETHERPAD_DB_NAME:=etherpad}
 
-ETHERPAD_DB_NAME=$( echo $ETHERPAD_DB_NAME | sed 's/\./_/g' )
+if [ -n "$ETHERPAD_DB_PASSWORD_FILE" ]; then
+	: ${ETHERPAD_DB_PASSWORD:=$(cat $ETHERPAD_DB_PASSWORD_FILE)}
+fi
 
 if [ -z "$ETHERPAD_DB_PASSWORD" ]; then
 	echo >&2 'error: missing required ETHERPAD_DB_PASSWORD environment variable'
 	echo >&2 '  Did you forget to -e ETHERPAD_DB_PASSWORD=... ?'
 	echo >&2
-	echo >&2 '  (Also of interest might be ETHERPAD_DB_USER and ETHERPAD_DB_NAME.)'
-
-	#exit 1
+	echo >&2 '  (Also of interest might be ETHERPAD_DB_PASSWORD_FILE, ETHERPAD_DB_USER and ETHERPAD_DB_NAME.)'
+	exit 1
 fi
 
 : ${ETHERPAD_TITLE:=Etherpad}
 : ${ETHERPAD_PORT:=9001}
-: ${ETHERPAD_SESSION_KEY:=$(
-		node -p "require('crypto').randomBytes(32).toString('hex')")}
 
-## Check if database already exists
-#RESULT=`mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} \
-#	-hmysql --skip-column-names \
-#	-e "SHOW DATABASES LIKE '${ETHERPAD_DB_NAME}'"`
-#
-#if [ "$RESULT" != $ETHERPAD_DB_NAME ]; then
-#	# mysql database does not exist, create it
-#	echo "Creating database ${ETHERPAD_DB_NAME}"
-#
-#	mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} -hmysql \
-#	      -e "create database ${ETHERPAD_DB_NAME}"
-#fi
+# Check if database already exists
+if [ "$ETHERPAD_DB_TYPE" == 'mysql' ]; then
+	RESULT=`mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} \
+		-h${ETHERPAD_DB_HOST} --skip-column-names \
+		-e "SHOW DATABASES LIKE '${ETHERPAD_DB_NAME}'"`
+
+	if [ "$RESULT" != $ETHERPAD_DB_NAME ]; then
+		# mysql database does not exist, create it
+		echo "Creating database ${ETHERPAD_DB_NAME}"
+
+		mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} -h${ETHERPAD_DB_HOST} \
+		      -e "create database ${ETHERPAD_DB_NAME}"
+	fi
+fi
+if [ "$ETHERPAD_DB_TYPE" == 'postgres' ]; then
+	export PGPASSWORD=${ETHERPAD_DB_PASSWORD}
+	if psql -U ${ETHERPAD_DB_USER} -h ${ETHERPAD_DB_HOST} postgres -lqt | cut -d \| -f 1 | grep -qw ${ETHERPAD_DB_NAME}; then
+		true
+	else
+		# postgresql database does not exist, create it
+		echo "Creating database ${ETHERPAD_DB_NAME}"
+		psql -U ${ETHERPAD_DB_USER} -h ${ETHERPAD_DB_HOST} postgres \
+			-c "create database ${ETHERPAD_DB_NAME}"
+	fi
+fi
 
 if [ ! -f settings.json ]; then
 
@@ -52,13 +63,13 @@ if [ ! -f settings.json ]; then
 	  "title": "${ETHERPAD_TITLE}",
 	  "ip": "0.0.0.0",
 	  "port" :${ETHERPAD_PORT},
-	  "sessionKey" : "${ETHERPAD_SESSION_KEY}",
-	  "dbType" : "mysql",
+	  "dbType" : "${ETHERPAD_DB_TYPE}",
 	  "dbSettings" : {
 			    "user"    : "${ETHERPAD_DB_USER}",
-			    "host"    : "${MYSQL_PORT_3306_TCP_ADDR}",
+			    "host"    : "${ETHERPAD_DB_HOST}",
 			    "password": "${ETHERPAD_DB_PASSWORD}",
-			    "database": "${ETHERPAD_DB_NAME}"
+			    "database": "${ETHERPAD_DB_NAME}",
+			    "charset": "${ETHERPAD_DB_CHARSET}"
 			  },
 	EOF
 
@@ -80,9 +91,5 @@ if [ ! -f settings.json ]; then
 	}
 	EOF
 fi
-
-#cat settings.json
-#cat /etc/hosts
-#ip a
 
 exec "$@"
